216074d37dbf2de3a9ebb6940c04c2db1ceaf13d
tibebe22 <tibebebirhanu@gmail.com>
2025-03-14 13:26:00 +0900

    Add files via upload

---
+++ header/head.h
@@ -0,0 +1,18 @@
+// Author: Hang-Hyun Jo
+// Description: master header file
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+#include<stdint.h>
+
+#include "memory.h"
+#include "MersenneTwister.h"
+#include "sort.h"
+
+
+
+//#include "analysis.h"
+#include "tree.h"
+#include "analysis.h"
